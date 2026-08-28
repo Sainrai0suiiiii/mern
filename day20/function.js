8d3eb16f1be9ec5e111dff813029ca44ce8d09ad
Sain Rai <sahinkulung@gmail.com>
2026-08-28 21:25:47 +0545

    decleration function

---
+++ day20/function.js
@@ -0,0 +1,36 @@
+function greet (name){
+    console.log("Hello " + name);
+}
+greet("sain");
+
+function say(name){
+    console.log("k hare " + name);
+}
+say("kale");
+
+function hey (kill){
+    console.log("kalo "+ kill);
+}
+hey("sale");
+
+function sain(great){
+    console.log("be great again "+ great);
+}
+sain("sain!!");
+
+function Rai(save){
+    console.log("save your culture " + save);
+}
+Rai("sain");
+
+function sahin(youFirst){
+    console.log("save yourself first "+ youFirst);
+}
+sahin("sain");
+
+function kina(chor){
+    console.log("Dont act chor "+ chor);
+}
+kina("juwade");
+
+
