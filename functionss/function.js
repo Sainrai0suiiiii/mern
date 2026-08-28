4f182724df37a157be1fd722e3012bd3d6b17264
Sain Rai <sahinkulung@gmail.com>
2026-08-28 23:03:04 +0545

    functions

---
+++ functionss/function.js
@@ -0,0 +1,69 @@
+function greet (name){
+    console.log("Hello " + name);
+}
+greet("sain");
+
+function say(name){
+    console.log("k hare " + name);
+}
+say("kale");
+
+function hey (kill){
+    console.log("kalo "+ kill);
+}
+hey("sale");
+
+function sain(great){
+    console.log("be great again "+ great);
+}
+sain("sain!!");
+
+function Rai(save){
+    console.log("save your culture " + save);
+}
+Rai("sain");
+
+function sahin(youFirst){
+    console.log("save yourself first "+ youFirst);
+}
+sahin("sain");
+
+function kina(chor){
+    console.log("Dont act chor "+ chor);
+}
+kina("juwade");
+
+function sandhya (beautiful){
+    console.log("she is beautiful "  + beautiful);
+}
+sandhya("meri maya!!");
+
+// function sandhya ma beautiful is a parameter which is defined as the variable defined passed during function declaration
+// sandhya("meri maya") is the arguments which is defined as actual value
+
+
+function addition(add){
+    console.log("this is addition of " + add);
+}
+addition(5,4);
+
+function jord(a,b){
+    console.log("this is the addition " + (a+b));
+}
+jord(9,10);
+
+function guna(c,d){
+    console.log("this is the multiplication of " + (c*d));
+}
+guna(5,2);
+
+function divison(e,f){
+    console.log("this is division "+(e/f));
+}
+divison(10,2);
+
+function modulo(x,y){
+    console.log("modolu " + (x%y));
+}
+modulo(8,3)
+
