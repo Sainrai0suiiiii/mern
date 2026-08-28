b2ede1ed866ca496020cfa536413aab9c841f621
Sain Rai <sahinkulung@gmail.com>
2026-08-28 23:30:01 +0545

    function expression

---
+++ functionss/functionExpression.js
@@ -0,0 +1,12 @@
+const add = function (a,b){
+    return a+b;
+};
+console.log(add(1,2));
+
+const multiply = function(a,b){
+    return a * b;
+};
+console.log(multiply(4,3));
+
+let z = multiply(4,33);
+console.log(z);
