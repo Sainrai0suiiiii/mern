e2980a35ed034c5f5068422de7dc59dda91c0bf1
Sain Rai <sahinkulung@gmail.com>
2026-08-29 08:57:08 +0545

    funciton

--- functionss/functionExpression.js
+++ functionss/functionExpression.js
@@ -10,3 +10,48 @@ console.log(multiply(4,3));
 
 let z = multiply(4,33);
 console.log(z);
+
+const hero = function(saymyname){
+    return saymyname;
+};
+console.log(hero("kale"));
+
+const zero = function(kiro){
+    return kiro;
+};
+console.log(zero("kitre"));
+
+const pagal = function(mahapagal){
+    return mahapagal;
+};
+console.log(pagal("ta hos muji"));
+
+const kaluram = function(bajelayRakhdeko){
+    return bajelayRakhdeko;
+};
+console.log(kaluram("heroxa"));
+
+const jiji = function(kaluram,seturam){
+    return (kaluram + seturam);
+};
+console.log("this is the kaluram and seturam age",jiji(5,6));
+
+const kiki = function (ee,pp){
+    return (ee * pp);
+};
+console.log("this is multiplication",kiki(44,55));
+
+const rectangle = function(l,b){
+    return (l*b);
+};
+console.log("area of rectangle",rectangle(12,8),"square");
+
+const square = function(s){
+    return( s*s);
+};
+console.log(square(8));
+
+const volCube=function(ss){
+    return(ss*ss*ss);
+};
+console.log("this is the volume of cube",volCube(8),"centimeter cube");
