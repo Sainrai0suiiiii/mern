321875b95a3c9cb77401496a2af2f58e48c67cba
Sain Rai <sahinkulung@gmail.com>
2026-08-30 23:21:47 +0545

    callback function

--- functionss/callBackFunction.js
+++ functionss/callBackFunction.js
@@ -16,3 +16,51 @@ function saysay(){
     console.log("bye");
 }
 say("sain",saysay);
+
+function hey (hey1, hey2){
+    console.log("heyhey "+ hey1);
+    hey2();
+}
+
+function ney(){
+    console.log("cheychye");
+}
+hey("sain Rai",ney);
+
+function below(tala,mathi){
+    console.log("down"+tala);
+    mathi();
+}
+function upward(){
+    console.log("chupLag");
+}
+below("chupmuji",upward);
+
+function sain(sahin,callback){
+    console.log("hello" + sahin);
+    callback();
+}
+function rai (){
+    console.log("RAi is king");
+}
+sain("Sahinkulung",rai);
+
+function rai1 (){
+    console.log("he is king");
+}
+rai1();
+
+function kulung(clan,subClan){
+    console.log("I belong to rai clan "+ clan);
+    subClan();
+}
+function Rai(){
+    console.log("My caste is Rai");
+}
+kulung("kulung is my clan",Rai);
+
+console.log("start");
+setTimeout(function(){
+    console.log("Inside setTimeout");
+},2000);
+console.log("end");
