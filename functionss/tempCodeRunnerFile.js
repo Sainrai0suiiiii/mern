2cd366f032bb6ffd24855a61c15180bdfa6d4e69
Sain Rai <sahinkulung@gmail.com>
2026-08-30 23:22:02 +0545

    tempt

---
+++ functionss/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+console.log("start");
+setTimeout(function(){
+    console.log("Inside setTimeout");
+},2000);
+console.log("end");
+
+console.log("suru");
+setTimeout(function(){
+    console.log("suru......");
+},100);
+console.log("samapta");
